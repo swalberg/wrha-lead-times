1f6366ff7a3d98fdcaded5c71bc86f72b899c177
Sean Walberg <sean@ertw.com>
2013-09-26 13:27:20 +0000

    Initial commit

---
+++ scrape.rb
@@ -0,0 +1,19 @@
+require 'nokogiri'
+require 'open-uri'
+
+def to_minutes(string)
+  (hr, min) = string.split(/\s?:\s?/)
+  hr.to_i * 60 + min.to_i
+end
+
+doc = Nokogiri::HTML(open('http://www.wrha.mb.ca/wait-times/'))
+
+time = doc.css('td.DateTime').inner_text
+time = Time.parse(time)
+
+doc.css('tr td span.Facility').each do |hospital|
+  row = hospital
+  row = row.parent while row.name != "tr"
+  (name, num_waiting, average_wait, _, longest_wait) = row.css('span').map(&:inner_text)
+  puts "#{time.strftime("%s")},\"#{name}\",#{num_waiting},#{to_minutes average_wait},#{to_minutes longest_wait}"
+end
